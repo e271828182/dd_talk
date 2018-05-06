9b02906bd38ffdc7c08e3de0be64211d213807c5
wangshumin <wangshumin@dianwoba.com>
2018-05-06 15:46:53 +0800

    ++

--- group_chat_robot/img_msg/matplotlib_demo.py
+++ group_chat_robot/img_msg/matplotlib_demo.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-matplotlib.rcParams['axes.unicode_minus'] = False
-fig, ax = plt.subplots()
-ax.plot(10*np.random.randn(100), 10*np.random.randn(100), 'o')
-ax.set_title('Using hyphen instead of Unicode minus')
+plt.plot(np.arange(10))
 # plt.show()
 plt.savefig("123.png")
-
