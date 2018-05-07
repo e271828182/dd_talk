264bd610a8b0d8d2ac42fc15e60c3468493945e3
wangshumin <wangshumin@dianwoba.com>
2018-05-07 11:43:39 +0800

    ++

--- remote_execute_jupyter/dd_websocket.py
+++ remote_execute_jupyter/dd_websocket.py
@@ -21,15 +21,17 @@ def on_close(ws):
 
 def on_open(ws):
     ws.send(json.dumps(["set_size", 54, 132, 869, 636]))
-    ws.send(json.dumps([u"stdin", u"cd /home/yangyang04/notebooks\r"]))
-    ws.send(json.dumps([u"stdin",
-                        u"jupyter nbconvert --ExecutePreprocessor.timeout=600  --execute 钉钉机器人2.ipynb --to notebook --inplace\r"]))
+    ws.send(json.dumps([u"stdin", u"cd /home/wangshumin/notebooks/dd_talk\r"]))
+    # ws.send(json.dumps([u"stdin", u"jupyter nbconvert --ExecutePreprocessor.timeout=600  --execute remote_test_py2.ipynb --to notebook --inplace\r"]))
+    # ws.send(json.dumps([u"stdin", u"jupyter trust remote_test_py2.ipynb\r"]))
+    ws.send(json.dumps([u"stdin", u"jupyter nbconvert --ExecutePreprocessor.kernel_name=python3  --execute remote_test_py3.ipynb --to notebook --inplace\r"]))
+    ws.send(json.dumps([u"stdin", u"jupyter trust remote_test_py3.ipynb\r"]))
     ws.send(json.dumps([u"stdin", u"exit\r"]))
 
 
 if __name__ == '__main__':
-    token = 'xxx'
-    url = "ws://jupyter.nidianwo.com/user/yangyang04/terminals/websocket/pao%d?token=%s" % (int(time.time()), token)
+    token = ''
+    url = "ws://jupyter.nidianwo.com/user/wangshumin/terminals/websocket/pao%d?token=%s" % (int(time.time()), token)
     ws = websocket.WebSocketApp(url,
                                 on_message=on_message,
                                 on_error=on_error,
